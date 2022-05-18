2dbc6a87503813ba3bd218931915297e55a00d81
Abdel <abdelkader.habel_pro@outlook.com>
2022-05-18 17:56:15 -0400

    all states appear on map when typed as input

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ turtle.shape(image)
 
 states=pd.read_csv("50_states.csv")
 state_list=states.state.to_list()
+guessed=[]
+while len(guessed)<len(state_list):
+    user_answer=screen.textinput(title=f"{len(guessed)}/{len(state_list)} termGuess a State",prompt="Enter a state")
+    user_answer=user_answer.title()
 
-user_answer=screen.textinput(title="Guess a State",prompt="Enter a state")
-print(states[states.state==user_answer.title()])
-
-print(states[states.state==user_answer.title()].x.item())
-
-if user_answer.title() in  state_list:
-    text=turtle.Turtle()
-    text.hideturtle()
-    text.penup()
-    x_coor=states[states.state==user_answer.title()].x.item()
-    y_coor=states[states.state==user_answer.title()].y.item()
-    text.goto(x_coor,y_coor)
-    print("goodguess")
+    if user_answer in  state_list:
+        guessed.append(user_answer)
+        text=turtle.Turtle()
+        text.hideturtle()
+        text.penup()
+        x_coor=states[states.state==user_answer].x.item()
+        y_coor=states[states.state==user_answer].y.item()
+        text.goto(x_coor,y_coor)
+        text.write(user_answer)
 
 screen.exitonclick()
